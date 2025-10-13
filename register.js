42d175b643be73f7da26a0085c8363edf1012815
Alejandro Sánchez Alejo <alejandro.alejo.snchz21@gmail.com>
2025-10-12 17:31:11 -0700

    Update register.js

--- register.js
+++ register.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyCdtOXIjjB_kL34DhJSGuaT5OWzw2i_A2E",
+  authDomain: "cas-tracker-dcfc4.firebaseapp.com",
+  projectId: "cas-tracker-dcfc4",
+  storageBucket: "cas-tracker-dcfc4.firebasestorage.app",
+  messagingSenderId: "782100915031",
+  appId: "1:782100915031:web:cd74ba7defa78927951412",
+  measurementId: "G-67K0LVYY2H"
 };
 
 firebase.initializeApp(firebaseConfig);
